64fe3a70cf24e441064fbce2897947998417ae09
Ladislav Matis <ladislav.matis@321sd.sk>
2025-02-02 14:21:00 +0100

    initial commit

---
+++ src/main/java/com/sonalake/meetup/SpringAiMeetupApplication.java
@@ -0,0 +1,13 @@
+package com.sonalake.meetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiMeetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiMeetupApplication.class, args);
+	}
+
+}
