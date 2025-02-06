3289cbc45ce64699903f91801eaa9de9fbbe1762
Ladislav Matis <ladislav.matis@321sd.sk>
2025-02-06 19:34:39 +0100

    Refactor Controller to use ChatClient for prompt handling.
    Replaced Prompt with ChatClient to streamline prompt handling logic.

--- src/main/java/com/sonalake/meetup/ai/examples/Controller.java
+++ src/main/java/com/sonalake/meetup/ai/examples/Controller.java
@@ -1,30 +1,25 @@
 package com.sonalake.meetup.ai.examples;
 
 import jakarta.annotation.PostConstruct;
-import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-
 @RequestMapping("/ai")
 public abstract class Controller {
+    private final ChatClient.Builder chatClientBuilder;
+    protected ChatClient chatClient;
+
     @Value("classpath:/prompts/system-prompt.st")
     protected Resource systemPromptResource;
 
-    protected Prompt systemPrompt;
+    public Controller(ChatClient.Builder chatClientBuilder) {
+        this.chatClientBuilder = chatClientBuilder;
+    }
 
-    /**
-     * Initializes the systemPrompt field by reading the content of the resource
-     * specified by the systemPromptResource property.
-     *
-     * @throws IOException if an I/O error occurs while reading the resource content
-     */
     @PostConstruct
-    public void init() throws IOException {
-        systemPrompt = new Prompt(systemPromptResource.getContentAsString(UTF_8));
+    public void init() {
+        chatClient = chatClientBuilder.defaultSystem(systemPromptResource).build();
     }
 }
