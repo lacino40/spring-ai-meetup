e7b3681062a6453874ab2c53b978c23efe552a52
Ladislav Matis <ladislav.matis@321sd.sk>
2025-02-14 13:01:48 +0100

    Enable WeatherProperties configuration in the application.
    Added the @EnableConfigurationProperties annotation to SpringAiMeetupApplication to support binding properties for WeatherProperties. This prepares the application to handle weather-based configurations effectively.

--- src/main/java/com/sonalake/meetup/SpringAiMeetupApplication.java
+++ src/main/java/com/sonalake/meetup/SpringAiMeetupApplication.java
@@ -1,9 +1,12 @@
 package com.sonalake.meetup;
 
+import com.sonalake.meetup.ai.service.WeatherProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(WeatherProperties.class)
 public class SpringAiMeetupApplication {
 
 	public static void main(String[] args) {
