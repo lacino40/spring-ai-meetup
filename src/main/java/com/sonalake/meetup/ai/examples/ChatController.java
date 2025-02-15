ce1d9c5cef3b18de964579fafb472dbe16a8e2af
Ladislav Matis <ladislav.matis@321sd.sk>
2025-02-15 13:26:13 +0100

    Add text-to-speech support in ChatController
    Introduced a TextToSpeechController dependency to enable text-to-speech functionality. Added a new endpoint, "/chat/tts", which processes user prompts, generates a chat response, and converts it to speech.

--- src/main/java/com/sonalake/meetup/ai/examples/ChatController.java
+++ src/main/java/com/sonalake/meetup/ai/examples/ChatController.java
@@ -13,12 +13,14 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/ai")
 public class ChatController {
     private final ChatClient chatClient;
+    private final TextToSpeechController textToSpeechController;
 
-    public ChatController(ChatClient chatClient) {
+    public ChatController(ChatClient chatClient, TextToSpeechController textToSpeechController) {
         this.chatClient = chatClient
                 .mutate()
                 .defaultAdvisors(new MessageChatMemoryAdvisor(new InMemoryChatMemory()))
                 .build();
+        this.textToSpeechController = textToSpeechController;
     }
 
     /**
@@ -35,5 +37,20 @@ public class ChatController {
                 .user(prompt)
                 .stream()
                 .content();
+    }
+
+    /**
+     * Handles a GET request to generate a chat response based on the provided user prompt,
+     * converts the response text to speech, and plays it asynchronously.
+     *
+     * @param prompt the user-provided prompt or input message, defaults to "Tell me a joke" if not provided
+     * @return the original chat response text generated
+     */
+    @GetMapping("chat/tts")
+    public String chatTextToSpeech(@RequestParam(defaultValue = "Tell me a joke", name = "prompt") String prompt) {
+
+        String text = chatClient.prompt().user(prompt).call().content();
+
+        return textToSpeechController.textToSpeech(text);
     }
 }
